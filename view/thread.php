7789dc2201b8eab8f32c711d95f1bba0ab47b1bd
4philip20 <philip.rippstein@iet-gibb.ch>
2017-09-19 14:00:21 +0200

    defintion of swag

---
+++ view/thread.php
@@ -0,0 +1,23 @@
+<div class="thpost">
+	<img class="bilder" src="/images/meme.jpg" alt="Bild Fehler" >
+</div>
+
+<div class="thpost">
+	<img class="thbilder" src="/images/meme2.jpg" alt="Bild Fehler" >
+</div>
+
+<div class="thpost">
+	<img class="thbilder" src="/images/meme3.jpg" alt="Bild Fehler" >
+</div>
+
+<div class="thpost">
+	<img class="thbilder" src="/images/meme4.jpg" alt="Bild Fehler" >
+</div>
+
+<div class="thpost">
+	<img class="thbilder" src="/images/meme5.jpg" alt="Bild Fehler" >
+</div>
+
+<div class="thpost">
+	<img class="thbilder" src="/images/meme6.jpg" alt="Bild Fehler" >
+</div>
